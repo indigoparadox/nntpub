c11a3902368e5e2dddd865f1d17f8c024798006f
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-03-29 13:45:03 -0400

    Rough message listing and retrieval.

--- nntpub/nntprotocol.py
+++ nntpub/nntprotocol.py
@@ -1,8 +1,10 @@
 
 import logging
 import asyncio
+from datetime import datetime
 
-NNTP_STATE_INIT = 0
+class NNTPNotFoundException( Exception ):
+    pass
 
 class NNTPServer( object ):
 
@@ -13,10 +15,13 @@ class NNTPServer( object ):
         self.server_port = server_port
         if 'domain' in kwargs:
             self.domain = kwargs['domain']
-        self.groups = [
-            {'name': 'control', 'desc': 'News server internal group'},
-            {'name': 'junk', 'desc': 'News server internal group'}
-        ]
+        self.groups = {
+            'control': {'desc': 'News server internal group', 'msg': []},
+            'junk': {'desc': 'News server internal group', 'msg': [
+                {'id': 7, 'subject': 'foo', 'body': 'test msg 1\r\nyo!', 'from': 'fromguy'},
+                {'id': 8, 'subject': 'fii', 'body': 'test msg 2\r\nhello!', 'from': 'fromotherguy'}
+            ]}
+        }
 
     async def listen( self ):
         self.server = await asyncio.start_server( self._handle_client, self.server_address, self.server_port )
@@ -25,21 +30,100 @@ class NNTPServer( object ):
 
     def send_banner( self, wfile ):
         # TODO: Indicate if posting allowed with 200.
-        wfile.write( b'201 ' + self.domain.encode( 'utf8' ) + b' NNTPub\r\n' )
+        NNTPServer._write_line( wfile, f'201 %s NNTPub' % self.domain )
 
     def send_list( self, wfile ):
 
-        wfile.write( b'215 Descriptions in form "group description"\r\n' )
+        NNTPServer._write_line( wfile, '215 Descriptions in form "group description"' )
         for group in self.groups:
-            wfile.write( group['name'].encode( 'utf8' ) + b' ' + \
-                group['desc'].encode( 'utf8' ) + b'\r\n' )
-        wfile.write( b'.\r\n' )
+            NNTPServer._write_line( wfile, f'%s %s' % (group, self.groups[group]['desc']) )
+        NNTPServer._write_line( wfile, '.' )
+
+    def send_group_meta( self, wfile, group_name : str ):
+        if not group_name in self.groups:
+            self.logger.warning( 'invalid group requested: %s', group_name )
+            NNTPServer._write_line( wfile, '411 No such group' )
+            raise NNTPNotFoundException
+        
+        group_len = len( self.groups[group_name]['msg'] )
+        if 0 >= group_len:
+            NNTPServer._write_line( wfile, f'211 0 0 0 %s' % (group_name,) )
+            return
+        
+        NNTPServer._write_line( wfile, f'211 %d %d %d %s' % \
+            (group_len, 
+            self.groups[group_name]['msg'][0]['id'], 
+            self.groups[group_name]['msg'][-1]['id'], 
+            group_name) )
+        
+    def send_group_list( self, wfile, group_name: str, start : int, end : int = -1 ):
+        if not group_name:
+            self.logger.warning( 'no group selected' )
+            NNTPServer._write_line( wfile, '412 No newsgroup currently selected' )
+            raise NNTPNotFoundException
+        
+        NNTPServer._write_line( wfile, '224 Overview information follows' )
+        
+        in_range = False
+        for msg in self.groups[group_name]['msg']:
+            if start == msg['id'] or (-1 == start and end == msg['id']):
+                in_range = True
+            if in_range:
+                # TODO: Lines field should count lines?
+                NNTPServer._write_line( wfile, '\t'.join(
+                    [NNTPServer._clean_field( x ) for x in \
+                        [str( msg['id'] ), msg['subject'], msg['from'], str( datetime.now() ),
+                        f'<%d@%s>' % (msg['id'], self.domain), '',
+                            str( len( NNTPServer._clean_field( msg['body'] ) ) ), '1']] ) )
+            if end == msg['id']:
+                in_range = False
+                
+        NNTPServer._write_line( wfile, '.' )
+
+    def send_article( self, wfile, group_name : str, msg_id : int ):
+        if not group_name:
+            self.logger.warning( 'no group selected' )
+            NNTPServer._write_line( wfile, '412 No newsgroup currently selected' )
+            raise NNTPNotFoundException
+        
+        # TODO: More efficient ID retrieval.
+        for msg in self.groups[group_name]['msg']:
+            if msg['id'] != msg_id:
+                continue
+
+            self._write_line( wfile, f'220 %d <%d@%s>' % (msg['id'], msg['id'], self.domain) )
+            self._write_line( wfile, f'From: %s' % (msg['from'],) )
+            self._write_line( wfile, f'Subject: %s' % (msg['subject'],) )
+            self._write_line( wfile, f'Date: %s' % (str( datetime.now()),) )
+            self._write_line( wfile, f'Message-ID: <%d@%s>' % (msg['id'],self.domain) )
+            self._write_line( wfile, '' )
+            for line in msg['body'].split( '\r\n' ):
+                self._write_line( wfile, line )
+            self._write_line( wfile, '.' )
+
+            # Message displayed; go home!
+            return
+        
+        # Could not find message.
+        self.logger.warning( 'invalid message number: %d', msg_id )
+        NNTPServer._write_line( wfile, '423 No message with that number' )
+        raise NNTPNotFoundException
+
+    @staticmethod
+    def _clean_field( field : str ):
+        return field.replace( '\r\n', '' ).replace( '\t', ' ' )
+
+    @staticmethod
+    def _write_line( wfile, line : str ):
+        logger = logging.getLogger( 'nntproto.line' )
+        logger.debug( line )
+        wfile.write( line.encode( 'utf8' ) + b'\r\n' )
 
     async def _handle_client( self, rfile, wfile ):
     
         line = None
         running = True
-        nntp_state = NNTP_STATE_INIT
+        nntp_group = None
 
         self.send_banner( wfile )
 
@@ -55,8 +139,49 @@ class NNTPServer( object ):
                 self.send_list( wfile )
 
             elif line.startswith( 'LIST' ):
-                self.logger.debug( 'unsupported list: %s', line )
-                wfile.write( b'503 Data item not stored' )
+                self.logger.warning( 'unsupported list: %s', line )
+                NNTPServer._write_line( wfile, '503 Data item not stored' )
+
+            elif line.startswith( 'GROUP' ):
+                line_arr = line.split( ' ' )
+                self.logger.debug( 'group info requested: %s', line )
+                try:
+                    self.send_group_meta( wfile, line_arr[1] )
+                    nntp_group = line_arr[1]
+                except NNTPNotFoundException:
+                    pass
+            
+            elif line.startswith( 'XOVER' ):
+                # TODO: Santize inputs.
+                self.logger.debug( 'group list requested: %s', line )
+                line_arr = line.split( ' ' )
+                if line_arr[1].endswith( '-' ):
+                    # Grab all messages remaining.
+                    try:
+                        self.send_group_list( wfile, nntp_group, int( line_arr[1] ) )
+                    except NNTPNotFoundException:
+                        pass
+                elif '-' in line_arr[1]:
+                    # Send a specific range.
+                    line_range = line_arr[1].split( '-' )
+                    try:
+                        self.send_group_list( wfile, nntp_group, int( line_range[0] ), end=int( line_range[1] ) )
+                    except NNTPNotFoundException:
+                        pass
+                else:
+                    # Send a single message.
+                    try:
+                        self.send_group_list( wfile, nntp_group, -1, end=int( line_arr[1] ) )
+                    except NNTPNotFoundException:
+                        pass
+
+            elif line.startswith( 'ARTICLE' ):
+                self.logger.debug( 'group list requested: %s', line )
+                line_arr = line.split( ' ' )
+                try:
+                    self.send_article( wfile, nntp_group, int( line_arr[1] ) )
+                except NNTPNotFoundException:
+                    pass
 
             elif '' != line:
                 logging.warning( 'unknown command: "%s"', line )
