c57e15104ed68425e621730d43263d76be14639a
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-03-29 15:24:13 -0400

    Track client state in its own object.

--- nntpub/nntprotocol.py
+++ nntpub/nntprotocol.py
@@ -6,11 +6,32 @@ from datetime import datetime
 class NNTPNotFoundException( Exception ):
     pass
 
+class NNTPClientHandler( object ):
+
+    def __init__( self,
+        rfile : asyncio.streams.StreamReader,
+        wfile : asyncio.streams.StreamWriter
+    ):
+        # TODO: Client ID.
+        self.logger = logging.getLogger( 'nntproto.client' )
+        self.rfile = rfile
+        self.wfile = wfile
+        self.running = True
+        self.nntp_group = None
+    
+    def change_group( self, group_name : str ):
+        self.logger.debug( 'using group: %s', group_name )
+        self.nntp_group = group_name
+
+    def write_line( self, line : str ):
+        self.logger.debug( 'writing: %s', line )
+        self.wfile.write( line.encode( 'utf8' ) + b'\r\n' )
+
 class NNTPServer( object ):
 
     def __init__( self, server_address, server_port, **kwargs ):
         self.server_address = server_address
-        self.logger = logging.getLogger( 'nntproto' )
+        self.logger = logging.getLogger( 'nntproto.server' )
         self.domain = 'nntp.example.com'
         self.server_port = server_port
         if 'domain' in kwargs:
@@ -28,41 +49,41 @@ class NNTPServer( object ):
         async with self.server:
             await self.server.serve_forever()
 
-    def send_banner( self, wfile ):
+    def send_banner( self, client : NNTPClientHandler ):
         # TODO: Indicate if posting allowed with 200.
-        NNTPServer._write_line( wfile, f'201 %s NNTPub' % self.domain )
+        client.write_line( f'201 %s NNTPub' % self.domain )
 
-    def send_list( self, wfile ):
+    def send_list( self, client : NNTPClientHandler ):
 
-        NNTPServer._write_line( wfile, '215 Descriptions in form "group description"' )
+        client.write_line( '215 Descriptions in form "group description"' )
         for group in self.groups:
-            NNTPServer._write_line( wfile, f'%s %s' % (group, self.groups[group]['desc']) )
-        NNTPServer._write_line( wfile, '.' )
+            client.write_line( f'%s %s' % (group, self.groups[group]['desc']) )
+        client.write_line( '.' )
 
-    def send_group_meta( self, wfile, group_name : str ):
+    def send_group_meta( self, client : NNTPClientHandler, group_name : str ):
         if not group_name in self.groups:
             self.logger.warning( 'invalid group requested: %s', group_name )
-            NNTPServer._write_line( wfile, '411 No such group' )
+            client.write_line( '411 No such group' )
             raise NNTPNotFoundException
         
         group_len = len( self.groups[group_name]['msg'] )
         if 0 >= group_len:
-            NNTPServer._write_line( wfile, f'211 0 0 0 %s' % (group_name,) )
+            client.write_line( f'211 0 0 0 %s' % (group_name,) )
             return
         
-        NNTPServer._write_line( wfile, f'211 %d %d %d %s' % \
+        client.write_line( f'211 %d %d %d %s' % \
             (group_len, 
             self.groups[group_name]['msg'][0]['id'], 
             self.groups[group_name]['msg'][-1]['id'], 
             group_name) )
         
-    def send_group_list( self, wfile, group_name: str, start : int, end : int = -1 ):
+    def send_group_list( self, client : NNTPClientHandler, group_name: str, start : int, end : int = -1 ):
         if not group_name:
             self.logger.warning( 'no group selected' )
-            NNTPServer._write_line( wfile, '412 No newsgroup currently selected' )
+            client.write_line( '412 No newsgroup currently selected' )
             raise NNTPNotFoundException
         
-        NNTPServer._write_line( wfile, '224 Overview information follows' )
+        client.write_line( '224 Overview information follows' )
         
         in_range = False
         for msg in self.groups[group_name]['msg']:
@@ -70,7 +91,7 @@ class NNTPServer( object ):
                 in_range = True
             if in_range:
                 # TODO: Lines field should count lines?
-                NNTPServer._write_line( wfile, '\t'.join(
+                client.write_line( '\t'.join(
                     [NNTPServer._clean_field( x ) for x in \
                         [str( msg['id'] ), msg['subject'], msg['from'], str( datetime.now() ),
                         f'<%d@%s>' % (msg['id'], self.domain), '',
@@ -78,12 +99,12 @@ class NNTPServer( object ):
             if end == msg['id']:
                 in_range = False
                 
-        NNTPServer._write_line( wfile, '.' )
+        client.write_line( '.' )
 
-    def send_article( self, wfile, group_name : str, msg_id : int ):
+    def send_article( self, client : NNTPClientHandler, group_name : str, msg_id : int ):
         if not group_name:
             self.logger.warning( 'no group selected' )
-            NNTPServer._write_line( wfile, '412 No newsgroup currently selected' )
+            client.write_line( '412 No newsgroup currently selected' )
             raise NNTPNotFoundException
         
         # TODO: More efficient ID retrieval.
@@ -91,62 +112,55 @@ class NNTPServer( object ):
             if msg['id'] != msg_id:
                 continue
 
-            self._write_line( wfile, f'220 %d <%d@%s>' % (msg['id'], msg['id'], self.domain) )
-            self._write_line( wfile, f'From: %s' % (msg['from'],) )
-            self._write_line( wfile, f'Subject: %s' % (msg['subject'],) )
-            self._write_line( wfile, f'Date: %s' % (str( datetime.now()),) )
-            self._write_line( wfile, f'Message-ID: <%d@%s>' % (msg['id'],self.domain) )
-            self._write_line( wfile, '' )
+            client.write_line( f'220 %d <%d@%s>' % (msg['id'], msg['id'], self.domain) )
+            client.write_line( f'From: %s' % (msg['from'],) )
+            client.write_line( f'Subject: %s' % (msg['subject'],) )
+            client.write_line( f'Date: %s' % (str( datetime.now()),) )
+            client.write_line( f'Message-ID: <%d@%s>' % (msg['id'],self.domain) )
+            client.write_line( '' )
             for line in msg['body'].split( '\r\n' ):
-                self._write_line( wfile, line )
-            self._write_line( wfile, '.' )
+                client.write_line( line )
+            client.write_line( '.' )
 
             # Message displayed; go home!
             return
         
         # Could not find message.
         self.logger.warning( 'invalid message number: %d', msg_id )
-        NNTPServer._write_line( wfile, '423 No message with that number' )
+        client.write_line( '423 No message with that number' )
         raise NNTPNotFoundException
 
     @staticmethod
     def _clean_field( field : str ):
         return field.replace( '\r\n', '' ).replace( '\t', ' ' )
 
-    @staticmethod
-    def _write_line( wfile, line : str ):
-        logger = logging.getLogger( 'nntproto.line' )
-        logger.debug( line )
-        wfile.write( line.encode( 'utf8' ) + b'\r\n' )
-
     async def _handle_client( self, rfile, wfile ):
     
         line = None
-        running = True
-        nntp_group = None
+        client = NNTPClientHandler( rfile, wfile )
 
-        self.send_banner( wfile )
+        self.send_banner( client )
 
-        while running:
+        while client.running:
             line = (await rfile.readline())
             line = line.decode( 'utf8' ).strip()
             if 'MODE READER' == line:
                 self.logger.debug( 'capabilities requested' )
-                self.send_banner( wfile )
+                self.send_banner( client )
 
             elif 'LIST' == line:
                 self.logger.debug( 'list requested' )
-                self.send_list( wfile )
+                self.send_list( client )
 
             elif line.startswith( 'LIST' ):
                 self.logger.warning( 'unsupported list: %s', line )
-                NNTPServer._write_line( wfile, '503 Data item not stored' )
+                client.write_line( '503 Data item not stored' )
 
             elif line.startswith( 'GROUP' ):
                 line_arr = line.split( ' ' )
                 self.logger.debug( 'group info requested: %s', line )
                 try:
-                    self.send_group_meta( wfile, line_arr[1] )
+                    self.send_group_meta( client, line_arr[1] )
                     nntp_group = line_arr[1]
                 except NNTPNotFoundException:
                     pass
@@ -158,20 +172,20 @@ class NNTPServer( object ):
                 if line_arr[1].endswith( '-' ):
                     # Grab all messages remaining.
                     try:
-                        self.send_group_list( wfile, nntp_group, int( line_arr[1] ) )
+                        self.send_group_list( client, nntp_group, int( line_arr[1] ) )
                     except NNTPNotFoundException:
                         pass
                 elif '-' in line_arr[1]:
                     # Send a specific range.
                     line_range = line_arr[1].split( '-' )
                     try:
-                        self.send_group_list( wfile, nntp_group, int( line_range[0] ), end=int( line_range[1] ) )
+                        self.send_group_list( client, nntp_group, int( line_range[0] ), end=int( line_range[1] ) )
                     except NNTPNotFoundException:
                         pass
                 else:
                     # Send a single message.
                     try:
-                        self.send_group_list( wfile, nntp_group, -1, end=int( line_arr[1] ) )
+                        self.send_group_list( client, nntp_group, -1, end=int( line_arr[1] ) )
                     except NNTPNotFoundException:
                         pass
 
@@ -179,7 +193,7 @@ class NNTPServer( object ):
                 self.logger.debug( 'group list requested: %s', line )
                 line_arr = line.split( ' ' )
                 try:
-                    self.send_article( wfile, nntp_group, int( line_arr[1] ) )
+                    self.send_article( client, nntp_group, int( line_arr[1] ) )
                 except NNTPNotFoundException:
                     pass
 
