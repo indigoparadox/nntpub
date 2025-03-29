2da3d0b5fffbd08fa23288008d81ca6fe6cea33a
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-03-29 11:41:41 -0400

    Rough config and debugging.

--- nntpub/__main__.py
+++ nntpub/__main__.py
@@ -1,10 +1,47 @@
 
+import argparse
+import logging
 import asyncio
+from configparser import ConfigParser
 
 from .nntprotocol import NNTPServer
 
 def main():
-    server = NNTPServer( '0.0.0.0' )
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument( '-c', '--config', default='nntpub.ini' )
+
+    parser.add_argument( '-v', '--verbose', action='store_true' )
+
+    args = parser.parse_args()
+
+    # Setup logging.
+    level = logging.INFO
+    if args.verbose:
+        level = logging.DEBUG
+    logging.basicConfig( level=level )
+    logger = logging.getLogger( 'main' )
+
+    # Setup config.
+    config = ConfigParser()
+    config.read( args.config )
+
+    listen_addr = '0.0.0.0'
+    try:
+        listen_addr = config['nntp']['listen']
+    except:
+        logger.warning( 'no listen address specified; using %s', listen_addr )
+
+    listen_port = 119
+    try:
+        listen_port = config['nntp']['port']
+    except:
+        logger.warning( 'no listen port specified; using %d', listen_port )
+
+    # Setup server.
+    config_arr = dict( config['nntp'] )
+    server = NNTPServer( listen_addr, listen_port, **config_arr )
     asyncio.run( server.listen() )
 
 if '__main__' == __name__:
--- nntpub/nntprotocol.py
+++ nntpub/nntprotocol.py
@@ -1,26 +1,38 @@
 
+import logging
 import asyncio
 
 NNTP_STATE_INIT = 0
 
 class NNTPServer( object ):
 
-    def __init__( self, server_address ):
+    def __init__( self, server_address, server_port, **kwargs ):
         self.server_address = server_address
+        self.logger = logging.getLogger( 'nntproto' )
+        self.domain = 'nntp.example.com'
+        self.server_port = server_port
+        if 'domain' in kwargs:
+            self.domain = kwargs['domain']
+        self.groups = [
+            {'name': 'control', 'desc': 'News server internal group'},
+            {'name': 'junk', 'desc': 'News server internal group'}
+        ]
 
     async def listen( self ):
-        self.server = await asyncio.start_server( self._handle_client, self.server_address, 60119 )
+        self.server = await asyncio.start_server( self._handle_client, self.server_address, self.server_port )
         async with self.server:
             await self.server.serve_forever()
 
     def send_banner( self, wfile ):
-        wfile.write( b'200 news.nntpub.example.com NNTPub news\r\n' )
+        # TODO: Indicate if posting allowed with 200.
+        wfile.write( b'201 ' + self.domain.encode( 'utf8' ) + b' NNTPub\r\n' )
 
     def send_list( self, wfile ):
 
         wfile.write( b'215 Descriptions in form "group description"\r\n' )
-        wfile.write( b'control News server internal group\r\n' )
-        wfile.write( b'junk News server internal group\r\n' )
+        for group in self.groups:
+            wfile.write( group['name'].encode( 'utf8' ) + b' ' + \
+                group['desc'].encode( 'utf8' ) + b'\r\n' )
         wfile.write( b'.\r\n' )
 
     async def _handle_client( self, rfile, wfile ):
@@ -35,10 +47,16 @@ class NNTPServer( object ):
             line = (await rfile.readline())
             line = line.decode( 'utf8' ).strip()
             if 'MODE READER' == line:
+                self.logger.debug( 'capabilities requested' )
                 self.send_banner( wfile )
 
-            if 'LIST' == line:
+            elif 'LIST' == line:
+                self.logger.debug( 'list requested' )
                 self.send_list( wfile )
 
+            elif line.startswith( 'LIST' ):
+                self.logger.debug( 'unsupported list: %s', line )
+                wfile.write( b'503 Data item not stored' )
+
             elif '' != line:
-                print( line )
+                logging.warning( 'unknown command: "%s"', line )
